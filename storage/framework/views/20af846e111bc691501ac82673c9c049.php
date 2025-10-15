3eba966d81cd172e45f4165b4c00f24dd1b2d426
JasonAdriel <abidano.24150@student.unud.ac.id>
2025-10-16 04:39:17 +0800

    Add files via upload

---
+++ storage/framework/views/20af846e111bc691501ac82673c9c049.php
@@ -0,0 +1,10 @@
+<!doctype html> 
+<html> 
+<head> 
+<meta charset="utf-8"> 
+<title>Hello Laravel</title> 
+</head> 
+<body> 
+<h1><?php echo e($message); ?></h1> 
+</body> 
+</html><?php /**PATH C:\Users\LENOVO\kampus-laravel\resources\views/hello.blade.php ENDPATH**/ ?>
